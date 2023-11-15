b4d3b6beb8695e0ae80044c222f5c73a0c998b6a
G4merGeld <67011875+G4merGeld@users.noreply.github.com>
2023-11-15 02:52:13 +0100

    made SinoTibetian inherit from Language modified constructor handled exception where name chinese

--- src/SinoTibetian.java
+++ src/SinoTibetian.java
@@ -1,2 +1,11 @@
-public class SinoTibetian {
+public class SinoTibetian extends Language{
+
+    public SinoTibetian(String name, int numSpeakers) {
+        super(name, numSpeakers, "Asia", "subject-object-verb");
+        if(this.name.contains("Chinese") || this.name.contains("chinese")){ //chinese sinotibetian languages have different wordorder
+            this.wordOrder = "subject-verb-object";
+        }
+    }
+
+
 }
