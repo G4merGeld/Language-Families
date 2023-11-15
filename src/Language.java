b1906141c750fe5ed6f17f0e5ff5494eb7d8bd65
G4merGeld <67011875+G4merGeld@users.noreply.github.com>
2023-11-15 02:26:50 +0100

    creating attributes and constructor to Language class

--- src/Language.java
+++ src/Language.java
@@ -1,2 +1,16 @@
-public class Language {
+public class Language { //parent class
+    protected String name;
+    protected int numSpeakers;
+    protected String regionsSpoken;
+    protected String wordOrder;
+
+    Language(String name, int numSpeakers, String regionsSpoken, String wordOrder){
+        this.name = name;
+        this.numSpeakers = numSpeakers;
+        this.regionsSpoken = regionsSpoken;
+        this. wordOrder = wordOrder;
+    }
+    public static void main(String[] args) {
+
+    }
 }
